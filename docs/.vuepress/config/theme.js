4f547465948d3e434532cbd3913a990505b972fa
qqlcx5 <qqlcx5@163.com>
2022-06-16 15:41:31 +0800

    build: prototype

--- docs/.vuepress/config/theme.js
+++ docs/.vuepress/config/theme.js
@@ -1,22 +1,23 @@
 const { defaultTheme } = require('@vuepress/theme-default')
 const navbar = require('./theme/navbar')
 const sidebar = require('./theme/sidebar')
+const { path } = require('@vuepress/utils')
 
-// const path = require('path') // 路径模块
-// const sourceDir = path.join(__dirname, '..', '..') // docs相对路径
-// const setFrontmatter = require('../utils/setFrontmatter')
-// const getSidebarData = require('../utils/getSidebarData')
-
-// setFrontmatter(sourceDir, {})
-// const sidebarData = getSidebarData(sourceDir, (collapsable = true))
-// console.log('sidebarData-------', JSON.stringify(sidebarData))
-module.exports = defaultTheme({
-    navbar, // 导航栏配置
-    sidebar, // 侧边栏配置
-    logo: 'https://feyoudao.oss-cn-hongkong.aliyuncs.com/site/favicon.ico', // 导航栏logo
-    contributors: false, // 是否显示贡献者
-    docsDir: 'docs',
-    docsBranch: 'master',
-    docsRepo: 'https://github.com/qqlcx5/vuepress-next',
-    editLinkPattern: ':repo/edit/:branch/:path'
-})
+// console.log(defaultTheme())
+module.exports = {
+    // 继承默认主题
+    extends: defaultTheme({
+        navbar, // 导航栏配置
+        sidebar, // 侧边栏配置
+        logo: 'https://feyoudao.oss-cn-hongkong.aliyuncs.com/site/favicon.ico', // 导航栏logo
+        contributors: false, // 是否显示贡献者
+        docsDir: 'docs',
+        docsBranch: 'master',
+        docsRepo: 'https://github.com/qqlcx5/vuepress-next',
+        editLinkPattern: ':repo/edit/:branch/:path'
+    }),
+    // 覆盖 `404` 布局
+    layouts: {
+        404: path.resolve(__dirname, '../theme/lib/client/layouts/404.vue')
+    }
+}
---
+++ docs/.vuepress/theme/lib/client/layouts/404.vue
@@ -0,0 +1,82 @@
+<script setup lang="ts">
+import { useRouteLocale } from '@vuepress/client'
+import { useThemeLocaleData } from '@vuepress/theme-default/lib/client/composables'
+ 
+const routeLocale = useRouteLocale()
+const themeLocale = useThemeLocaleData()
+
+const homeLink = themeLocale.value.home ?? routeLocale.value
+const homeText = themeLocale.value.backToHome ?? 'Back to home'
+</script>
+
+<template>
+    <div class="theme-container pkfrom-container">
+        <div class="theme-default-content">
+            <div class="warpper">
+                <img
+                    src="https://feyoudao.oss-cn-hongkong.aliyuncs.com/site/404-light.gif"
+                    alt="404 error"
+                />
+                <!-- end Rabbit -->
+                <p class="desc">The page you are looking for was moved, removed,<br />
+                    renamed or might never existed.</p>
+                <RouterLink
+                    class="btn"
+                    :to="homeLink"
+                >{{ homeText }}</RouterLink>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.pkfrom-container {
+    min-height: 100vh;
+    width: 100%;
+    background-color: #eaff6f;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.info {
+    position: relative;
+    z-index: 999;
+    margin-top: -12vh;
+    font-family: 'Raleway', Helvetica, Arial, sans-serif;
+    font-weight: 300;
+    text-align: center;
+}
+
+.info .desc {
+    line-height: 1.6rem;
+    color: #1b334d;
+    margin: 1rem 0;
+}
+.btn {
+    font-size: 18px;
+    font-weight: 600;
+    color: #ffffff;
+    border: 0px solid;
+    border-bottom: 2px solid;
+    border-color: #ec9228;
+    padding: 10px 41px;
+    border-radius: 5px;
+    background: none;
+    text-transform: uppercase;
+    display: inline-block;
+    background-color: #ffad32;
+    -webkit-transition: all 0.5s ease-in-out;
+    -moz-transition: all 0.5s ease-in-out;
+    -ms-transition: all 0.5s ease-in-out;
+    -o-transition: all 0.5s ease-in-out;
+    transition: all 0.5s ease-in-out;
+    text-decoration: none;
+    cursor: pointer;
+}
+.btn:hover {
+    background: #ec9228;
+    color: #ffffff;
+    text-decoration: none;
+}
+</style>
