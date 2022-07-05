4804a24ba6b2c739f1b74b417c3ed7adbd761a1c
qqlcx5 <qqlcx5@163.com>
2022-07-05 17:47:46 +0800

    build: prototype

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -1,23 +1,19 @@
+// const { path } = require('@vuepress/utils')
 const { googleAnalyticsPlugin } = require('@vuepress/plugin-google-analytics')
-const { mediumZoomPlugin } = require('@vuepress/plugin-medium-zoom')
 const { searchPlugin } = require('@vuepress/plugin-search')
 const { registerComponentsPlugin } = require('@vuepress/plugin-register-components')
 // const typed = require('../plugins/vuepress-plugin-typed')
-const { path } = require('@vuepress/utils')
+const { mediumZoomPlugin } = require('@vuepress/plugin-medium-zoom')
 const { copyCodePlugin } = require('vuepress-plugin-copy-code2')
 const { photoSwipePlugin } = require('vuepress-plugin-photo-swipe')
 const { mdEnhancePlugin } = require('vuepress-plugin-md-enhance')
 const { commentPlugin } = require('vuepress-plugin-comment2')
 const { readingTimePlugin } = require('vuepress-plugin-reading-time2')
-const { sitemapPlugin } = require("vuepress-plugin-sitemap2");
+const { sitemapPlugin } = require('vuepress-plugin-sitemap2')
 
 module.exports = [
     googleAnalyticsPlugin({
         id: 'G-7Q9JVXXQBW'
-    }),
-    // <a> 标签以外的所有图片不包括.no-zoom都支持缩放
-    mediumZoomPlugin({
-        selector: ':not(a) > img:not(.no-zoom)'
     }),
     searchPlugin({
         maxSuggestions: 20,
@@ -26,10 +22,14 @@ module.exports = [
         // 允许搜索 Frontmatter 中的 `tags` `categories`
         getExtraFields: page => (page.frontmatter.tags || page.frontmatter.categories) ?? []
     }),
+    // <a> 标签以外的所有图片不包括.no-zoom都支持缩放
+    // mediumZoomPlugin({
+    //     selector: ':not(a) > img:not(.no-zoom)'
+    // }),
     // 全局组件注册
-    registerComponentsPlugin({
-        componentsDir: path.resolve(__dirname, '../components')
-    }),
+    // registerComponentsPlugin({
+    //     componentsDir: path.resolve(__dirname, '../components')
+    // }),
     // copyCodePlugin({
     //     showInMobile: false
     // }),
@@ -43,16 +43,16 @@ module.exports = [
     // readingTimePlugin({
     //     // 你的选项
     // }),
-    // sitemapPlugin({
-    //     hostname: "feyoudao.cn",
-    // }),
-    // mdEnhancePlugin({
-    //     tabs: true, // 添加选项卡支持
-    //     codetabs: true, // 启用代码块分组
-    //     imageMark: true, // 启用图片标记
-    //     imageSize: true, // 启用图片大小
-    //     include: true, // 启用导入支持
-    //     demo: true, // 启用代码演示
-    //     presentation: true // 启用幻灯片
-    // })
+    sitemapPlugin({
+        hostname: "feyoudao.cn",
+    }),
+    mdEnhancePlugin({
+        tabs: true, // 添加选项卡支持
+        codetabs: true, // 启用代码块分组
+        imageMark: true, // 启用图片标记
+        imageSize: true, // 启用图片大小
+        include: true, // 启用导入支持
+        demo: true, // 启用代码演示
+        presentation: true // 启用幻灯片
+    })
 ]
