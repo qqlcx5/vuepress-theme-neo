78618b19c01d7e6bcc613bc7eef1cb45a0e3829c
qqlcx5 <qqlcx5@163.com>
2022-08-10 09:38:52 +0800

    build: prototype

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -33,48 +33,46 @@ module.exports = [
     // }),
     docsearchPlugin({
         appId: 'BXJ3ODS43J',
-        apiKey: '76c82d7ad20d1045db580721acdc4cb8',
+        apiKey: '454eb8558182c5133b92b8cd465cbb7d',
         indexName: 'feyoudao',
         debug: true,
         locales: {
-            '/': {
-                placeholder: '搜索文档',
-                translations: {
-                    button: {
-                        buttonText: '搜索文档',
-                        buttonAriaLabel: '搜索文档'
+            placeholder: '搜索文档1',
+            translations: {
+                button: {
+                    buttonText: '搜索文档2',
+                    buttonAriaLabel: '搜索文档3'
+                },
+                modal: {
+                    searchBox: {
+                        resetButtonTitle: '清除查询条件',
+                        resetButtonAriaLabel: '清除查询条件',
+                        cancelButtonText: '取消',
+                        cancelButtonAriaLabel: '取消'
                     },
-                    modal: {
-                        searchBox: {
-                            resetButtonTitle: '清除查询条件',
-                            resetButtonAriaLabel: '清除查询条件',
-                            cancelButtonText: '取消',
-                            cancelButtonAriaLabel: '取消'
-                        },
-                        startScreen: {
-                            recentSearchesTitle: '搜索历史',
-                            noRecentSearchesText: '没有搜索历史',
-                            saveRecentSearchButtonTitle: '保存至搜索历史',
-                            removeRecentSearchButtonTitle: '从搜索历史中移除',
-                            favoriteSearchesTitle: '收藏',
-                            removeFavoriteSearchButtonTitle: '从收藏中移除'
-                        },
-                        errorScreen: {
-                            titleText: '无法获取结果',
-                            helpText: '你可能需要检查你的网络连接'
-                        },
-                        footer: {
-                            selectText: '选择',
-                            navigateText: '切换',
-                            closeText: '关闭',
-                            searchByText: '搜索提供者'
-                        },
-                        noResultsScreen: {
-                            noResultsText: '无法找到相关结果',
-                            suggestedQueryText: '你可以尝试查询',
-                            reportMissingResultsText: '你认为该查询应该有结果？',
-                            reportMissingResultsLinkText: '点击反馈'
-                        }
+                    startScreen: {
+                        recentSearchesTitle: '搜索历史',
+                        noRecentSearchesText: '没有搜索历史',
+                        saveRecentSearchButtonTitle: '保存至搜索历史',
+                        removeRecentSearchButtonTitle: '从搜索历史中移除',
+                        favoriteSearchesTitle: '收藏',
+                        removeFavoriteSearchButtonTitle: '从收藏中移除'
+                    },
+                    errorScreen: {
+                        titleText: '无法获取结果',
+                        helpText: '你可能需要检查你的网络连接'
+                    },
+                    footer: {
+                        selectText: '选择',
+                        navigateText: '切换',
+                        closeText: '关闭',
+                        searchByText: '搜索提供者'
+                    },
+                    noResultsScreen: {
+                        noResultsText: '无法找到相关结果',
+                        suggestedQueryText: '你可以尝试查询',
+                        reportMissingResultsText: '你认为该查询应该有结果？',
+                        reportMissingResultsLinkText: '点击反馈'
                     }
                 }
             }
@@ -83,7 +81,7 @@ module.exports = [
     commentPlugin({
         provider: 'Waline',
         emoji: ['https://unpkg.com/@waline/emojis@1.0.1/bilibili', 'https://unpkg.com/@waline/emojis@1.0.1/tieba'],
-        serverURL: 'https://vuepress-waline-psi.vercel.app/',
+        serverURL: 'https://vuepress-waline-psi.vercel.app/'
         // provider: 'Twikoo',
         // envId: 'https://twikoo-qqlcx5.vercel.app/',
     }),
