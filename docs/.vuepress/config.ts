688765568e07fd49490d901b8f23a495a504b4c7
qqlcx5 <qqlcx5@163.com>
2022-08-16 17:09:26 +0800

    build: prototype

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -7,7 +7,7 @@ import theme from './config/theme'
 export default defineUserConfig({
     lang: 'zh-CN',
     title: '前端有道',
-    base: '/vuepress-next/',
+    // base: '/vuepress-next/',
     description: '前端有道,技术博客,前端,前端开发,前端博客,前端框架,web前端,前端面试,技术文档,学习,面试,JavaScript,js,es6,typeScript,ts,vue,python,css3,html5,node,git,github,markdown,vuepress,react',
     head,
     theme,
