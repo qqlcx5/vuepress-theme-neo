a502329cbefbac7aa303af48d149c705cfd0693d
qqlcx5 <qqlcx5@163.com>
2022-09-06 23:22:26 +0800

    build: prototype

--- docs/.vuepress/config/sidebar.js
+++ docs/.vuepress/config/sidebar.js
@@ -1,5 +1,6 @@
 import { getDirname, path } from '@vuepress/utils'
 const __dirname = getDirname(import.meta.url) // import and use the dirname() method from the path module.
+console.log('sidebar __dirname---------------------', __dirname);
 const readFile = require('../utils/simpleReadFile') // 读取文件函数模块
 const docsRoot = path.resolve(__dirname, '..', '..') // docs相对路径
 
