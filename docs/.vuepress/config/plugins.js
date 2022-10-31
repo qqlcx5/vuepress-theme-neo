886e0e6955710e75ceb55a8cce7eb997257708c9
qqlcx5 <qqlcx5@163.com>
2022-10-31 18:05:33 +0800

    build: prototype

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -31,51 +31,65 @@ export default [
     // registerComponentsPlugin({
     //     componentsDir: path.resolve(__dirname, '../components')
     // }),
+    // docsearchPlugin({
+    //     appId: 'BXJ3ODS43J',
+    //     apiKey: '454eb8558182c5133b92b8cd465cbb7d',
+    //     indexName: 'feyoudao',
+    //     debug: true,
+    //     locales: {
+    //         placeholder: '搜索文档1',
+    //         translations: {
+    //             button: {
+    //                 buttonText: '搜索文档2',
+    //                 buttonAriaLabel: '搜索文档3'
+    //             },
+    //             modal: {
+    //                 searchBox: {
+    //                     resetButtonTitle: '清除查询条件',
+    //                     resetButtonAriaLabel: '清除查询条件',
+    //                     cancelButtonText: '取消',
+    //                     cancelButtonAriaLabel: '取消'
+    //                 },
+    //                 startScreen: {
+    //                     recentSearchesTitle: '搜索历史',
+    //                     noRecentSearchesText: '没有搜索历史',
+    //                     saveRecentSearchButtonTitle: '保存至搜索历史',
+    //                     removeRecentSearchButtonTitle: '从搜索历史中移除',
+    //                     favoriteSearchesTitle: '收藏',
+    //                     removeFavoriteSearchButtonTitle: '从收藏中移除'
+    //                 },
+    //                 errorScreen: {
+    //                     titleText: '无法获取结果',
+    //                     helpText: '你可能需要检查你的网络连接'
+    //                 },
+    //                 footer: {
+    //                     selectText: '选择',
+    //                     navigateText: '切换',
+    //                     closeText: '关闭',
+    //                     searchByText: '搜索提供者'
+    //                 },
+    //                 noResultsScreen: {
+    //                     noResultsText: '无法找到相关结果',
+    //                     suggestedQueryText: '你可以尝试查询',
+    //                     reportMissingResultsText: '你认为该查询应该有结果？',
+    //                     reportMissingResultsLinkText: '点击反馈'
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }),
     docsearchPlugin({
-        appId: 'BXJ3ODS43J',
-        apiKey: '454eb8558182c5133b92b8cd465cbb7d',
-        indexName: 'feyoudao',
-        debug: true,
+        apiKey: 'VUE9LSPBO8',
+        indexName: '9d972c785b0197e79df852a925cbfe88',
         locales: {
-            placeholder: '搜索文档1',
-            translations: {
-                button: {
-                    buttonText: '搜索文档2',
-                    buttonAriaLabel: '搜索文档3'
-                },
-                modal: {
-                    searchBox: {
-                        resetButtonTitle: '清除查询条件',
-                        resetButtonAriaLabel: '清除查询条件',
-                        cancelButtonText: '取消',
-                        cancelButtonAriaLabel: '取消'
-                    },
-                    startScreen: {
-                        recentSearchesTitle: '搜索历史',
-                        noRecentSearchesText: '没有搜索历史',
-                        saveRecentSearchButtonTitle: '保存至搜索历史',
-                        removeRecentSearchButtonTitle: '从搜索历史中移除',
-                        favoriteSearchesTitle: '收藏',
-                        removeFavoriteSearchButtonTitle: '从收藏中移除'
-                    },
-                    errorScreen: {
-                        titleText: '无法获取结果',
-                        helpText: '你可能需要检查你的网络连接'
-                    },
-                    footer: {
-                        selectText: '选择',
-                        navigateText: '切换',
-                        closeText: '关闭',
-                        searchByText: '搜索提供者'
-                    },
-                    noResultsScreen: {
-                        noResultsText: '无法找到相关结果',
-                        suggestedQueryText: '你可以尝试查询',
-                        reportMissingResultsText: '你认为该查询应该有结果？',
-                        reportMissingResultsLinkText: '点击反馈'
+            '/': {
+                placeholder: 'Search Documentation',
+                translations: {
+                    button: {
+                        buttonText: 'Search Documentation'
                     }
                 }
-            }
+            },
         }
     }),
     commentPlugin({
