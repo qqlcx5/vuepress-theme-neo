944cded06116eabfd14cdb747b8e7e6d4707aa18
lcx <qqlcx5@163.com>
2023-01-17 14:40:41 +0800

    build: prototype

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -6,7 +6,7 @@ import { defineUserConfig } from 'vuepress'
 export default defineUserConfig({
     lang: 'zh-CN',
     title: '前端有道',
-    description: '前端有道,技术博客,前端,前端开发,前端博客,前端框架,web前端,前端面试,技术文档,学习,面试,JavaScript,js,es6,typeScript,ts,vue,python,css3,html5,node,git,github,markdown,vuepress,react',
+    description: '前端有道、技术博客、前端、前端开发、前端博客、前端框架、web前端、前端面试、技术文档、学习、面试、JavaScript、js、es6、typeScript、ts、vue、python、css3、html5、node、git、github、markdown、vuepress、react',
     head: head as [],
     theme,
     plugins
