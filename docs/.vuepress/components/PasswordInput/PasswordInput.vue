e3c4cb646e3674daa682642d93d4f9e7f40d43b6
lcx <qqlcx5@163.com>
2023-02-16 14:21:20 +0800

    build: prototype

---
+++ docs/.vuepress/components/PasswordInput/PasswordInput.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="max-width">
+        <PasswordInput v-model="password" />
+    </div>
+</template>
+
+<script>
+import PasswordInput from './CmpPasswordInput.vue'
+
+export default {
+    name: 'PasswordInputDemo',
+    components: {
+        PasswordInput
+    }
+}
+</script>
+<style lang="scss" scoped>
+.max-width {
+    max-width: 360px;
+    margin: 20px auto 0;
+}
+</style>
--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -2,7 +2,7 @@
 import { googleAnalyticsPlugin } from '@vuepress/plugin-google-analytics'
 import { searchPlugin } from '@vuepress/plugin-search'
 import { docsearchPlugin } from '@vuepress/plugin-docsearch'
-// import { registerComponentsPlugin } from '@vuepress/plugin-register-components'
+import { registerComponentsPlugin } from '@vuepress/plugin-register-components'
 // import typed from '../plugins/vuepress-plugin-typed'
 // import { mediumZoomPlugin } from '@vuepress/plugin-medium-zoom'
 import { copyCodePlugin } from 'vuepress-plugin-copy-code2'
@@ -12,6 +12,8 @@ import { commentPlugin } from 'vuepress-plugin-comment2'
 // import { readingTimePlugin } from 'vuepress-plugin-reading-time2'
 import { sitemapPlugin } from 'vuepress-plugin-sitemap2'
 
+import { getDirname, path } from '@vuepress/utils'
+const __dirname = getDirname(import.meta.url)
 export default [
     googleAnalyticsPlugin({ id: 'G-7Q9JVXXQBW' }),
     searchPlugin({
@@ -26,9 +28,13 @@ export default [
     //     selector: ':not(a) > img:not(.no-zoom)'
     // }),
     // 全局组件注册
-    // registerComponentsPlugin({
-    //     componentsDir: path.resolve(__dirname, '../components')
-    // }),
+    registerComponentsPlugin({
+        getComponentName: filename => {
+            let name = filename.split('/')[0]
+            return path.trimExt(name.replace(/\/|\\/g, '-'))
+        },
+        componentsDir: path.resolve(__dirname, '../components')
+    }),
     commentPlugin({
         // provider: 'Waline',
         // emoji: ['https://unpkg.com/@waline/emojis@1.0.1/bilibili', 'https://unpkg.com/@waline/emojis@1.0.1/tieba'],
